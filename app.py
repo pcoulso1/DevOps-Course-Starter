d35506ee3306a68c612693cbdca6a552ca84991b
Peter Taylor-Coulson <pcoulson@btinternet.com>
2020-06-01 14:13:30 +0100

    Strtch goal objectives

--- app.py
+++ app.py
@@ -8,11 +8,29 @@ app.config.from_object('flask_config.Config')
 def index():
     return render_template('index.html', items=session.get_items())
 
-@app.route('/add', methods=['POST'])
+@app.route('/update/<id>')
+def update(id):
+    session.update_item(id)
+    return redirect("/")
+
+@app.route('/add', methods=['GET','POST'])
 def add():
     if request.method == 'POST':
-        session.add_item(request.form.get('new_todo'))
-        return render_template('index.html', items=session.get_items())
-    
+        if 'add' in request.form:
+            session.add_item(request.form.get('new_todo'))
+        return redirect("/")
+
+    return render_template('add.html', items=session.get_items())
+
+@app.route('/delete', methods=['POST'], defaults={'id': None})
+@app.route('/delete/<id>', methods=['GET'])
+def delete(id):
+    if request.method == 'POST':
+        if 'delete' in request.form:
+            session.remove_item(request.form.get('id'))
+        return redirect("/")
+
+    return render_template('delete.html', item=session.get_item(id))
+ 
 if __name__ == '__main__':
     app.run(debug=True)
