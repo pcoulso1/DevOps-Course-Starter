3e03ea5baa7da068d2d733d0d1caf9695b06f862
Peter Taylor-Coulson <pcoulson@btinternet.com>
2020-08-16 13:29:19 +0100

    Correcting the use of .env and .env.test when running tests in VS Code

--- tests/test_app_integration.py
+++ tests/test_app_integration.py
@@ -1,14 +1,15 @@
 import pytest
 from unittest import mock
 from dotenv import load_dotenv, find_dotenv
+
+# Use our test integration config instead of the 'real' version 
+file_path = find_dotenv('.env.test')
+load_dotenv(file_path, override=True)
+
 import app
 
 @pytest.fixture
 def client():
-    # Use our test integration config instead of the 'real' version 
-    file_path = find_dotenv('.env.test')
-    load_dotenv(file_path, override=True)
-
     test_app = app.create_app()
 
     with test_app.test_client() as client:
