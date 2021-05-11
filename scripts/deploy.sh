88710073e25099e7cecc1763b3623465ff6a586f
Peter Taylor-Coulson <pcoulson@btinternet.com>
2021-05-11 17:43:37 +0100

    Updates after review

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,4 +14,4 @@ terraform init
 terraform apply -auto-approve -var "github_client_id=$AZURE_GITHUB_CLIENT_ID" -var "github_client_secret=$AZURE_GITHUB_CLIENT_SECRET" -var "github_logon_redirect=$AZURE_GITHUB_LOGON_REDIRECT"
 
 # Call Azure web hoot to restart app
-curl -dH -X POST "$(printf %q $(terraform output -raw cd_webhook))"
+curl -dH -X POST "$(terraform output -raw cd_webhook)"
